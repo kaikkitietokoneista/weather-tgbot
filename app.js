f65e6fcf09f310de4d328c78e97f464bcb99eedd
Kaikkitietokoneista.net <52327670+kaikkitietokoneista@users.noreply.github.com>
2019-10-11 19:33:11 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const TelegramBot = require('node-telegram-bot-api');
+const weather = require('openweather-apis');
+
+/* CONFIGURE WEATHER*/
+weather.setLang('fi'); //Language
+
+weather.setCity('Helsinki'); //City
+
+weather.setUnits('metric');
+
+weather.setAPPID('OPENWEATHER_APPID'); //Get your own appid from openweather
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = 'TELEGRAM BOT TOKEN';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+
+  var command = "/weather";
+  if (msg.text.toString().toLowerCase().includes(command)) {
+    weather.getSmartJSON(function(err, json){
+      if(err) console.log(err);
+      console.log(json);
+      bot.sendMessage(msg.chat.id, "Temperature: " + json.temp + "°C\nDescription: " + json.description + "\nHumidity: " + json.humidity + "%" + "\nPressure: " + json.pressure + " hPa");
+    });
+  }
+
+});
