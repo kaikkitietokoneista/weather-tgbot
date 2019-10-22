17e829072639765ac0b4e2b53e6afe56c027563a
Kaikkitietokoneista.net <52327670+kaikkitietokoneista@users.noreply.github.com>
2019-10-22 15:38:05 +0000

    Updated reply and added country

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ var city = 'Helsinki';
 
 weather.setUnits('metric');
 
-weather.setAPPID('');
+weather.setAPPID('YOUR_OPENWEATHERMAP_APPID');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '';
+const token = 'YOUR_TELEGRAM_BOT_API';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -46,7 +46,7 @@ bot.onText(/\/weather (.+)/, (msg, match) => {
     }
     if (json.cod == "404") {
       bot.sendMessage(msg.chat.id, "City not found. 🧐");
-    } else {
+    }  else {
       //var imgurl = 'http://openweathermap.org/img/w/' + json.weather[0].icon + '.png';
       //bot.sendPhoto(msg.chat.id, imgurl)
 
@@ -58,28 +58,29 @@ bot.onText(/\/weather (.+)/, (msg, match) => {
         var weatheremoji = '⛅';
       }
       else if (json.weather[0].icon == "03d" || "03n") {
-        var weatheremoji = '☁️';
+        var weatheremoji = '☁';
       }
       else if (json.weather[0].icon == "04d"|| "04n") {
-        var weatheremoji = '☁️';
+        var weatheremoji = '☁';
       }
       else if (json.weather[0].icon == "09d" || "09n") {
-        var weatheremoji = '🌧️';
+        var weatheremoji = '🌧';
       }
       else if (json.weather[0].icon == "10d" || "10n") {
-        var weatheremoji = '🌧️';
+        var weatheremoji = '🌧';
       }
       else if (json.weather[0].icon == "11d" || "11n") {
-        var weatheremoji = '⛈️';
+        var weatheremoji = '⛈';
       }
       else if (json.weather[0].icon == "13d" || "13n") {
-        var weatheremoji = '❄️';
+        var weatheremoji = '❄';
       }
       else if (json.weather[0].icon == "50d" || "50n") {
-        var weatheremoji = '🌫️';
+        var weatheremoji = '🌫';
       }
 
-      bot.sendMessage(msg.chat.id, "City: " + city + "\n_____________________\nTemperature: " + json.main.temp + "°C🌡️\n_____________________\nDescription: " + json.weather[0].description + weatheremoji + "\n_____________________\nHumidity: " + json.main.humidity + "%" + "\n_____________________\nPressure: " + json.main.pressure + " hPa" + "\n_____________________\nWind: " + json.wind.speed + "m/s💨\n_____________________");
+      bot.sendMessage(msg.chat.id, "City: " + city + "\nTemperature: " + json.main.temp + "°C🌡\nDescription: " + json.weather[0].description + weatheremoji + "\nHumidity: " + json.main.humidity + "%" + "\nPressure: " + json.main.pressure + " hPa" + "\nWind: " + json.wind.speed + "m/s💨\nCountry: " + json.sys.country);
+      console.log(json);
     }
   });
 });
