87cca169930fbedc454b522a1abd2b6deae664e9
Kaikkitietokoneista.net <52327670+kaikkitietokoneista@users.noreply.github.com>
2019-10-28 20:02:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ var city = 'Helsinki';
 
 weather.setUnits('metric');
 
-weather.setAPPID('');
+weather.setAPPID('OPENWEATHERMAP_APPID');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '';
+const token = 'TELEGRAM_TOKEN';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -84,66 +84,14 @@ bot.onText(/\/weather (.+)/, (msg, match) => {
   });
 });
 
-bot.onText(/\/weather@ktweather_bot (.+)/, (msg, match) => {
-
-  const city = match[1]; // the captured "city"
-
-  weather.setCity(city);
-
-  weather.getAllWeather(function(err, json){
-    if(err) throw (err);
-    //console.log(json);
-
-    if (json.cod == "429") {
-      bot.sendMessage(msg.chat.id, "We have reached our API limit of current minute. 😔");
-    }
-    if (json.cod == "404") {
-      bot.sendMessage(msg.chat.id, "City not found. 🧐");
-    }  else {
-      //var imgurl = 'http://openweathermap.org/img/w/' + json.weather[0].icon + '.png';
-      //bot.sendPhoto(msg.chat.id, imgurl)
-
-      /* icon to emoji */
-      if (json.weather[0].icon == "01d") {
-        var weatheremoji = '🌞';
-      }
-      else if (json.weather[0].icon == "02d" || "02n") {
-        var weatheremoji = '⛅';
-      }
-      else if (json.weather[0].icon == "03d" || "03n") {
-        var weatheremoji = '☁️';
-      }
-      else if (json.weather[0].icon == "04d"|| "04n") {
-        var weatheremoji = '☁️';
-      }
-      else if (json.weather[0].icon == "09d" || "09n") {
-        var weatheremoji = '🌧️';
-      }
-      else if (json.weather[0].icon == "10d" || "10n") {
-        var weatheremoji = '🌧️';
-      }
-      else if (json.weather[0].icon == "11d" || "11n") {
-        var weatheremoji = '⛈️';
-      }
-      else if (json.weather[0].icon == "13d" || "13n") {
-        var weatheremoji = '❄️';
-      }
-      else if (json.weather[0].icon == "50d" || "50n") {
-        var weatheremoji = '🌫️';
-      }
-
-      bot.sendMessage(msg.chat.id, "City: " + city + "\nTemperature: " + json.main.temp + "°C🌡\nDescription: " + json.weather[0].description + weatheremoji + "\nHumidity: " + json.main.humidity + "%" + "\nPressure: " + json.main.pressure + " hPa" + "\nWind: " + json.wind.speed + "m/s💨\nCountry: " + json.sys.country);
-    }
-  });
-});
-
-
 bot.on('message', (msg) => {
-  //Lisää /weather Helsinki komennot tähän ja käyttämään tai (||) vertailijaa tai vaihda kaikki nämä onText iin
+  console.log(msg);
+
+  //Lisää /weather Helsinki komennot tähän ja käyttämään tai (||) vertailijaa
   if (msg.text.toString().toLowerCase().includes("/start" || "/start@ktweather_bot")) {
     bot.sendMessage(msg.chat.id, "This is an open source bot which tells you the current weather. This bot is made by kaikkitietokoneista.net/bots. You can view the source code of this bot in https://github.com/kaikkitietokoneista/weather-tgbot. \n\nCommands:\n/weather <cityname> - gives you the current weather in a chosen city\n/weather - gives you a list of the available cities");
   }
-  if (msg.text.toString().toLowerCase() == "/weather" || "/weather@ktweather_bot") {
+  if (msg.text.toString().toLowerCase() == "/weather") {
     bot.sendMessage(msg.chat.id, "Choose your city from a list or send it to here with syntax /weather yourcity.", {
       "reply_markup": {
         "keyboard": [["/weather Helsinki"], ["/weather New York"], ["/weather Berlin"], ["/weather Sydney"], ["/weather London"], ["/weather Beijing"], ["/weather Tokyo"], ["/weather Johannesburg"], ["/weather Cairo"]]
